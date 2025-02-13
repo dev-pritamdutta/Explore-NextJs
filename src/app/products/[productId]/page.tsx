728c85b4bc0dbae460cf61ff9f286fa73efae24c
pritam dutta <webdevpritam7498@gmail.com>
2025-02-13 20:37:21 +0600

    Nested Dynamic Routes

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,6 @@
 export default function ProductDetails({params} : { params: {productId: string} }){
     return <h1>Product Details of {params.productId}</h1>
 }
+// This is TypeScript syntax for defining the component’s props.
+// params is an object containing a property called productId, which is a string.
+// Next.js automatically provides params when using dynamic routes.
